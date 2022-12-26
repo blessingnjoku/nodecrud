eaa0a96e16e47b69c3783952144a4673fd058534
Blessing Njoku <blessingnjoku505@gmail.com>
2022-12-26 17:59:52 +0100

    first node commit

---
+++ User.js
@@ -0,0 +1,34 @@
+const users =[
+    {
+        id:1,
+        name:'blessing',
+        email:'blessingnjoku@gmail.com',
+        age:'20'
+    },
+    {
+        id:2,
+        name:'buki',
+        email:'buki@gmail.com',
+        age:'30'
+    },
+    {
+        id:3,
+        name:'vero',
+        email:'vero@gmail.com',
+        age:'29'
+    },
+    {
+        id:4,
+        name:'grace',
+        email:'grace@gmail.com',
+        age:'40'
+    },
+    {
+        id:5,
+        name:'chi',
+        email:'chi@gmail.com',
+        age:'27'
+    },
+]
+
+module.exports = users;
